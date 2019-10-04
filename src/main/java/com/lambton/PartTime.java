ce2e2e87dac302ab0562cb3a884bb70b4be69063
komal2006 <55511982+komal2006@users.noreply.github.com>
2019-10-04 17:44:06 -0400

    [Komal]:  class PartTime\ created constructor

--- src/main/java/com/lambton/PartTime.java
+++ src/main/java/com/lambton/PartTime.java
@@ -5,6 +5,8 @@ public class PartTime
     private double rate;
     private float hoursWorked;
 
-
-
+    public PartTime(double rate, float hoursWorked) {
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
+    }
 }
