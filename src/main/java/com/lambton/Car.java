2e20ac773be61b50bce71cbb322510d9fcdac820
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 17:58:51 -0400

    [Varinder] : Class car with getter and setter

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -1,6 +1,24 @@
 package com.lambton;
 
-public class Car {
+public class Car extends
+
+{
     private Double distanceTravelled;
     private String capacity;
+
+    
+
+    public Double getDistanceTravelled() {
+        return distanceTravelled;
+    }
+    public void setDistanceTravelled(Double distanceTravelled) {
+        this.distanceTravelled = distanceTravelled;
+    }
+    public String getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
 }
