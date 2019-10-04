974a06fc778ee6e86bc5a32f4d22b3f30f2b970c
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 15:42:24 -0400

    [Charan]: Car class getter and setter

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -5,4 +5,24 @@ public class Car
 
     private float distanceTravelled;
     private String capacity;
+    public Car(String make, String color, String plate, Double distanceTravelled, String capacity) {
+        super(make, color, plate);
+        this.distanceTravelled=distanceTravelled;
+        this.capacity=capacity;
+
+    }
+
+
+    public Double getDistanceTravelled() {
+        return distanceTravelled;
+    }
+    public void setDistanceTravelled(Double distanceTravelled) {
+        this.distanceTravelled = distanceTravelled;
+    }
+    public String getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
 }
