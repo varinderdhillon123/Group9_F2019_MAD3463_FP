4b14d6a4907e7e131f3b59b134ed93ec3ab93c7a
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 15:35:32 -0400

    [charan]:vehicle class

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -7,6 +7,36 @@ public class Vehicle {
         private String make;
         private String color;
         private String plate;
-        System.out.println("Checking branch charan");
+       // System.out.println("Checking branch charan");
+        public Vehicle(String make, String color,  String plate) {
+            this.make = make;
+            this.color = color;
+            this.plate = plate;
+
+        }
+
+        public String getMake() {
+            return make;
+        }
+
+        public void setMake(String make) {
+            this.make = make;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String getPlate() {
+            return plate;
+        }
+
+        public void setPlate(String plate) {
+            this.plate = plate;
+        }
     }
 }
